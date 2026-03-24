e51ad281ec26bd36f6fef0402654f76e2045fae7
Moises Victor <moisesvictorh16@gmail.com>
2026-03-24 20:53:48 -0300

    sistemas

---
+++ sistemas/Program.vb
@@ -0,0 +1,20 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+    
+        dim sistema(3) As String
+
+        sistema(0) = "linux"
+        sistema(1) = "windows"
+        sistema(2) = "ios"
+        
+        console.writeLine(sistema(0))
+
+        console.writeLine(sistema(1))
+
+        console.writeLine(sistema(2))
+
+
+    End Sub
+End Module
---
+++ vetor/Program.vb
@@ -0,0 +1,15 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+       
+        dim nomecompleto (2) As String
+
+       nomecompleto(0) = "weslen"
+       nomecompleto (1) = "henrique"
+
+       console.writeLine (nomecompleto (0))
+
+       console.writeLine (nomecompleto (1))
+    End Sub
+End Module
